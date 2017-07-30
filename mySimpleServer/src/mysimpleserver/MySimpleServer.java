4f541287e97efca6b08ed96380196c0d373a2234
Chris Chiang <cchiang1992@gmail.com>
2017-07-29 17:27:39 -0700

    Open socket to port and setup IO

--- mySimpleServer/src/mysimpleserver/MySimpleServer.java
+++ mySimpleServer/src/mysimpleserver/MySimpleServer.java
@@ -5,11 +5,21 @@
  */
 package mysimpleserver;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
 /**
  *
  * @author ChrisMac
  */
 public class MySimpleServer {
+  
+  final private int PORT = 8888;
 
   /**
    * @param args the command line arguments
@@ -17,6 +27,25 @@ public class MySimpleServer {
   public static void main(String[] args) {
     // TODO code application logic here
     System.out.println("Hello World.");
+    
+    new MySimpleServer();
+  }
+  
+  public MySimpleServer() {
+    try {
+      ServerSocket mServerSocket = new ServerSocket(PORT);
+      System.out.println("Server started at port: " + PORT);
+      
+      Socket mSocket = mServerSocket.accept();
+      
+      PrintWriter output = new PrintWriter(mSocket.getOutputStream(), true);
+      BufferedReader input = new BufferedReader(new InputStreamReader(mSocket.getInputStream()));
+      
+      output.println("Connected to server: " + new Date());
+      
+    } catch (IOException e) {
+      System.out.println("Error creating server: " + e);
+    }
   }
   
 }
